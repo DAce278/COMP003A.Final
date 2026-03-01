54e8d2f97f9685cda15138d3d9781f4a99648ca0
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-02-28 16:43:59 -0800

    Added first iteration of wanted string/int/bool fields in volunteer class.

--- COMP003A.Final/Volunteer.cs
+++ COMP003A.Final/Volunteer.cs
@@ -6,6 +6,30 @@ namespace COMP003A.Final
 {
     internal class Volunteer
     {
+        public string Name;
+        public string Race;
+        public string EyeColor;
+        public string HairColor;
+        public string Town;
+        public string AreaOfOrigin;
+        public string PreviousVolunteerPosition;
 
+        public int BirthYear;
+        public int Age;
+        public int Height;
+        public int ExpectedHours;
+        public int ExperienceMonths;
+        public int VolunteerHours;
+        public int Weight;
+        public int bodyMass;
+        public int WeightLift;
+        public int ApplicantNum;
+
+        public bool Sex;
+        public bool PreviouslyVolunteered;
+        public bool AttendingClasses;
+        public bool PhysCandidate;
+        public bool Over16;
     }
+
 }

9b34904570f86a3c29a17675f2863dc4b309a2f5
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-03-01 17:06:30 -0800

    Added menu loop, basic switch for menu, a list for volunteers, and WIP attempts of adding to list

--- COMP003A.Final/Program.cs
+++ COMP003A.Final/Program.cs
@@ -1,10 +1,81 @@
-﻿namespace COMP003A.Final
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+namespace COMP003A.Final
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Volunteer> volunteers = new List<Volunteer>();
+
+
+            // TEMPORARY
+            
+            
+            Console.WriteLine("-------------------------------------\n" +
+                              "Welcome to the Volunteer database!\n" +
+                              "-------------------------------------");
+            volunteers.Add(new Volunteer("name", "race", "eye", "hair", "town", "origin", "prevvolunteerwork", 2020, 18, 60, 69, 47, 457, 57, 57, 57, true, true, true, true));
+            while (true)
+            {
+                Console.Write("\n" +
+                              "\nPlease select an option:" +
+                              "\n1. Create New Profile" +
+                              "\n2. View All Profiles" +
+                              "\n3. Search Profiles" +
+                              "\n4. Summary Statistics" +
+                              "\n5. Exit" +
+                              "\nOption Selection: ");
+                string menuSelect = Console.ReadLine();
+                Console.WriteLine(volunteers[0].Name);
+
+
+                // user choices
+                switch (menuSelect)
+                {
+                    //Add
+                    case "1":
+                        
+                        continue;
+
+                    // View
+                    case "2":
+
+
+                        continue;
+
+                    // Search
+                    case "3":
+
+
+                        continue;
+
+                    // Stats
+                    case "4":
+
+
+                        continue;
+                        
+                    // Exit
+                    case "5":
+                        Console.WriteLine("\n\nGoodbye!");
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Unknown selection, please try again.");
+                        continue;
+                }
+
+                break;
+            }
+        }
+
+        public void Additions(List<Volunteer> volunteers)
+        {
+
+        volunteers.Add(new Volunteer("name", "race", "eye", "hair", "town", "origin", "prevvolunteerwork", 2020, 18, 60, 69, 47, 457, 57, 57, 57, true, true, true, true));
+       
         }
     }
 }
--- COMP003A.Final/Volunteer.cs
+++ COMP003A.Final/Volunteer.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace COMP003A.Final
 {
-    internal class Volunteer
+    public class Volunteer
     {
         public string Name;
         public string Race;
@@ -22,14 +22,59 @@ namespace COMP003A.Final
         public int VolunteerHours;
         public int Weight;
         public int bodyMass;
-        public int WeightLift;
         public int ApplicantNum;
 
         public bool Sex;
         public bool PreviouslyVolunteered;
         public bool AttendingClasses;
-        public bool PhysCandidate;
         public bool Over16;
+
+        public Volunteer(string name, string race, string eyeColor, string hairColor, string town, string areaOfOrigin, string previousVolunteerPosition, int birthYear, int age, int height, int expectedHours, int experienceMonths, int volunteerHours, int weight, int bodyMass, int applicantNum, bool sex, bool previouslyVolunteered, bool attendingClasses, bool over16)
+        {
+            Name = name;
+            Race = race;
+            EyeColor = eyeColor;
+            HairColor = hairColor;
+            Town = town;
+            AreaOfOrigin = areaOfOrigin;
+            PreviousVolunteerPosition = previousVolunteerPosition;
+            BirthYear = birthYear;
+            Age = age;
+            Height = height;
+            ExpectedHours = expectedHours;
+            ExperienceMonths = experienceMonths;
+            VolunteerHours = volunteerHours;
+            Weight = weight;
+            this.bodyMass = bodyMass;
+            ApplicantNum = applicantNum;
+            Sex = sex;
+            PreviouslyVolunteered = previouslyVolunteered;
+            AttendingClasses = attendingClasses;
+            Over16 = over16;
+
+     //       List<Volunteer> volunteers = new List<Volunteer>();
+      //      volunteers.Add(volunteer1);
+        }
+          //  Volunteer volunteer1 = new("name", "race", "eye", "hair", "town", "origin", "prevvolunteerwork", 2020, 18, 60, 69, 47, 457, 57, 57, 57, true, true, true, true);
+
+        
     }
 
+    public class Physical
+    {
+        public int WeightLift;
+        public bool PhysCandidate;
+
+        public Physical(int weightLift, bool physCandidate)
+        {
+            WeightLift = weightLift;
+            PhysCandidate = physCandidate;
+        }
+
+        public Physical()
+        {
+            Physical phys1 = new(234, true);
+        }
+
+    }
 }
